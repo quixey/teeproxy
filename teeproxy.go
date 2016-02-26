f12bd11ef29c9f82306cff80997357d5beda2a2e
Chris Hoffman <choffman@quixey.com>
2016-02-26 14:55:36 -0800

    Attempt to create https-able client for teeproxy

--- teeproxy.go
+++ teeproxy.go
@@ -19,7 +19,7 @@ var (
 	listen            = flag.String("l", ":8888", "port to accept requests")
 	targetProduction  = flag.String("a", "localhost:8080", "where production traffic goes. http://localhost:8080/production")
 	altTarget         = flag.String("b", "localhost:8081", "where testing traffic goes. response are skipped. http://localhost:8081/test")
-	debug             = flag.Bool("debug", true, "more logging, showing ignored output")
+	debug             = flag.Bool("debug", false, "more logging, showing ignored output")
 	productionTimeout = flag.Int("a.timeout", 3, "timeout in seconds for production traffic")
 	alternateTimeout  = flag.Int("b.timeout", 3, "timeout in seconds for alternate site traffic")
 )
@@ -46,30 +46,6 @@ func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 				fmt.Println("Recovered in f", r)
 			}
 		}()
-		/*// Open new TCP connection to the server
-		clientTcpConn, err := net.DialTimeout("tcp", h.Alternative, time.Duration(time.Duration(*alternateTimeout)*time.Second))
-		if err != nil {
-			if *debug {
-				fmt.Printf("Failed to connect to %s\n", h.Alternative)
-			}
-			return
-		}
-		clientHttpConn := httputil.NewClientConn(clientTcpConn, nil) // Start a new HTTP connection on it
-		defer clientHttpConn.Close()                                 // Close the connection to the server
-		err = clientHttpConn.Write(req1)                             // Pass on the request
-		if err != nil {
-			if *debug {
-				fmt.Printf("Failed to send to %s: %v\n", h.Alternative, err)
-			}
-			return
-		}
-		_, err = clientHttpConn.Read(req1) // Read back the reply
-		if err != nil {
-			if *debug {
-				fmt.Printf("Failed to receive from %s: %v\n", h.Alternative, err)
-			}
-			return
-		} */
 		p, err := LocalParseURL(h.Alternative)
 		if err != nil{
 			if *debug {
@@ -98,26 +74,6 @@ func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			fmt.Println("Recovered in f", r)
 		}
 	}()
-
-	// Open new TCP connection to the server
-	/*clientTcpConn, err := net.DialTimeout("tcp", h.Target, time.Duration(time.Duration(*productionTimeout)*time.Second))
-	if err != nil {
-		fmt.Printf("Failed to connect to %s\n", h.Target)
-		return
-	}
-
-	clientHttpConn := httputil.NewClientConn(clientTcpConn, nil) // Start a new HTTP connection on it
-	defer clientHttpConn.Close()                                 // Close the connection to the server
-	err = clientHttpConn.Write(req2)                             // Pass on the request
-	if err != nil {
-		fmt.Printf("Failed to send to %s: %v\n", h.Target, err)
-		return
-	}
-	resp, err := clientHttpConn.Read(req2) // Read back the reply
-	if err != nil {
-		fmt.Printf("Failed to receive from %s: %v\n", h.Target, err)
-		return
-	}*/
 	p, err := LocalParseURL(h.Target)
 	if err != nil{
 		fmt.Printf("Failed to parse Target: %s: %v\n", h.Target, err)
